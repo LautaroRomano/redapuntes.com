6a9d7216721454c6655cb245539b1b8bad82ec4a
LautaroRomano <71612453+LautaroRomano@users.noreply.github.com>
2024-05-23 01:52:29 -0300

    auth y login

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,88 @@
+import NextAuth from 'next-auth';
+
+import GoogleProvider from "next-auth/providers/google"
+import CredentialsProvider from "next-auth/providers/credentials";
+import conn from '@/app/lib/db';
+import bcrypt from 'bcryptjs'
+
+export const authOptions = {
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        }),
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: { label: "Email", type: "text", placeholder: "jsmith@mail.com" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials, req) {
+                const user = await getUser(credentials.username, credentials.password)
+                if (user) {
+                    return user
+                } else {
+                    return null
+                }
+            }
+        })
+    ],
+    callbacks: {
+        async signIn({ account, profile, credentials }) {
+            if (credentials) {
+                const user = await getUser(credentials.username, credentials.password)
+                if (user) {
+                    return user
+                } else {
+                    return null
+                }
+            }
+            return null
+        },
+        async redirect({ url, baseUrl }) {
+            if (url.startsWith("/")) return `${baseUrl}${url}`
+            else if (new URL(url).origin === baseUrl) return url
+            return baseUrl
+        }
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+    // pages: {
+    //     signIn: '/login',
+    //     error: '/login'
+    // },
+    url: process.env.NEXTAUTH_URL,
+}
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
+
+const getUser = async (username, password) => {
+    try {
+        const { rows: result } = await conn.query(
+            'SELECT * FROM users WHERE username=$1',
+            [username]
+        );
+        console.log("🚀 ~ getUser ~ result:", result)
+
+        if (result.length === 0) {
+            return null;
+        }
+
+        const user = result[0];
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+        if (isPasswordMatch) {
+            /* await conn.query(
+                'UPDATE users set last_connection = now() where id = $1',
+                [user.id]
+            ); */
+            return user;
+        } else {
+            return null;
+        }
+
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+};
--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,3 +1,4 @@
+'use client'
 import {
   Navbar as NextUINavbar,
   NavbarContent,
@@ -19,10 +20,17 @@ import {
   SearchIcon,
   Logo,
 } from "@/components/icons";
-
-import { FaUser } from "react-icons/fa";
+import { FaCheckCircle, FaUser } from "react-icons/fa";
+import { signIn, signOut, useSession } from 'next-auth/react';
+import { Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, useDisclosure } from "@nextui-org/react";
+import { useState } from "react";
 
 export const Navbar = () => {
+
+  const { data: session, status } = useSession();
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  console.log("🚀 ~ Navbar ~ session, status:", session, status)
+
   const searchInput = (
     <Input
       aria-label="Search"
@@ -40,60 +48,168 @@ export const Navbar = () => {
   );
 
   return (
-    <NextUINavbar maxWidth="xl" position="sticky">
-      <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
-        <NavbarBrand as="li" className="gap-3 max-w-fit">
-          <NextLink className="flex justify-start items-center gap-1" href="/">
-            <Logo />
-            <p className="font-bold text-inherit">Repositorio Universitario</p>
-          </NextLink>
-        </NavbarBrand>
-      </NavbarContent>
+    <>
+      <NextUINavbar maxWidth="xl" position="sticky">
+        <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
+          <NavbarBrand as="li" className="gap-3 max-w-fit">
+            <NextLink className="flex justify-start items-center gap-1" href="/">
+              <Logo />
+              <p className="font-bold text-inherit">Repositorio Universitario</p>
+            </NextLink>
+          </NavbarBrand>
+        </NavbarContent>
 
-      <NavbarContent
-        className="hidden sm:flex basis-1/5 sm:basis-full"
-        justify="end"
-      >
-        <NavbarItem className="hidden sm:flex gap-2">
-          <ThemeSwitch />
-        </NavbarItem>
-        <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
-        <NavbarItem className="hidden md:flex">
-          <Button
-            isExternal
-            as={Link}
-            className="text-sm font-normal text-default-600 bg-default-100"
-            href={'/profile'}
-            startContent={<FaUser />}
-            variant="flat"
-          >
-            Mi perfil
-          </Button>
-        </NavbarItem>
-      </NavbarContent>
+        <NavbarContent
+          className="hidden sm:flex basis-1/5 sm:basis-full"
+          justify="end"
+        >
+          <NavbarItem className="hidden sm:flex gap-2">
+            <ThemeSwitch />
+          </NavbarItem>
+          <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
+          <NavbarItem className="hidden md:flex">
 
-      <NavbarMenu>
-        {searchInput}
-        <div className="mx-4 mt-2 flex flex-col gap-2">
-          {siteConfig.navMenuItems.map((item, index) => (
-            <NavbarMenuItem key={`${item}-${index}`}>
-              <Link
-                color={
-                  index === 2
-                    ? "primary"
-                    : index === siteConfig.navMenuItems.length - 1
-                      ? "danger"
-                      : "foreground"
-                }
-                href="#"
-                size="lg"
+            {status === 'authenticated' ?
+              <Dropdown>
+                <DropdownTrigger>
+                  <Button
+                    className="text-sm font-normal text-default-600 bg-default-100"
+                    startContent={<FaUser />}
+                    variant="flat"
+                  >
+                    Cuenta
+                  </Button>
+                </DropdownTrigger>
+                <DropdownMenu aria-label="Dynamic Actions">
+
+                  <DropdownItem
+                    color={"default"}
+                    className={""}
+                  >
+                    <Link
+                      isExternal
+                      className="w-full h-full text-sm font-normal text-default-600 "
+                      href={'/profile'}
+                    >
+                      Ver perfil
+                    </Link>
+                  </DropdownItem>
+                  <DropdownItem
+                    color={"danger"}
+                    className={"text-danger"}
+                    onClick={signOut}
+                  >
+                    Cerrar sesion
+                  </DropdownItem>
+                </DropdownMenu>
+
+              </Dropdown>
+              :
+              <Button
+                className="text-sm font-normal text-default-600 bg-default-100"
+                variant="flat"
+                onClick={onOpenChange}
               >
-                {item.label}
-              </Link>
-            </NavbarMenuItem>
-          ))}
-        </div>
-      </NavbarMenu>
-    </NextUINavbar>
+                Iniciar sesion
+              </Button>
+            }
+
+          </NavbarItem>
+        </NavbarContent>
+
+        <NavbarMenu>
+          {searchInput}
+          <div className="mx-4 mt-2 flex flex-col gap-2">
+            {siteConfig.navMenuItems.map((item, index) => (
+              <NavbarMenuItem key={`${item}-${index}`}>
+                <Link
+                  color={
+                    index === 2
+                      ? "primary"
+                      : index === siteConfig.navMenuItems.length - 1
+                        ? "danger"
+                        : "foreground"
+                  }
+                  href="#"
+                  size="lg"
+                >
+                  {item.label}
+                </Link>
+              </NavbarMenuItem>
+            ))}
+          </div>
+        </NavbarMenu>
+      </NextUINavbar>
+      <Login isOpen={isOpen} onOpenChange={onOpenChange}></Login>
+    </>
+  );
+};
+
+
+const Login = ({ isOpen, onOpenChange }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [success, setSucces] = useState(false)
+  const [error, setError] = useState(false)
+
+  const handleSubmit = () => {
+    try {
+      signIn('credentials', { username, password })
+      setSucces(true)
+
+      setTimeout(() => {
+        setUsername("");
+        setPassword([]);
+        setSucces(false);
+        onOpenChange();
+      }, 1000);
+
+    } catch (error) {
+      setError(true)
+    }
+
+  };
+
+  return (
+    <Modal isOpen={isOpen} onOpenChange={onOpenChange} size="xl">
+      <ModalContent>
+        {(onClose) => (
+          <>
+            <ModalHeader>
+              <h4>Iniciar sesion</h4>
+            </ModalHeader>
+            <ModalBody>
+              <Input
+                placeholder="Usuario o email"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <Input
+                placeholder="Contrasena"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <NextLink className="text-blue-400" href={'#'}>Olvidaste tu contrasena?</NextLink>
+              <NextLink className="flex justify-start items-center gap-1" href="/register">No tienes cuenta? <p className="text-blue-400">Registrate aqui!</p></NextLink>
+            </ModalBody>
+
+            <ModalFooter>
+              <Button auto flat color="error" onPress={onOpenChange}>
+                Cancelar
+              </Button>
+              {success ?
+                <Button color="success" startContent={<FaCheckCircle />}>
+                  Listo
+                </Button>
+                :
+                <Button auto onPress={handleSubmit}>
+                  Ingresar
+                </Button>
+              }
+            </ModalFooter>
+          </>
+        )}
+      </ModalContent>
+    </Modal>
   );
 };
